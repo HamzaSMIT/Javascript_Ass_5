fc03c271ce32607d8eea4eda773396bce1dfe13d
HamzaSMIT <creatorbro135@gmail.com>
2023-05-17 00:55:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+                                                                // Chapter:35 to 38
+                                                                // Question: 01
+// function displayDateTime() {
+//     var currentDate = new Date();
+//     var dateTimeString = currentDate.toLocaleString();
+//     document.getElementById("datetime").innerText = dateTimeString;
+//   }
+
+                                                                // Question: 02
+// function greetUser(firstName, lastName) {
+//   var fullName = firstName + " " + lastName;
+//   console.log("Hello, " + fullName + "! Welcome!");
+// }
+// greetUser(prompt("Enter your First Name"), prompt("Enter your Last Name"));
+  
+                                                                // Question: 03
+// function addNum() {
+//   var num1 = prompt("Enter your 1st Number")
+//   var num2 = prompt("Enter your 2nd Number")
+
+//   var sums = Number(num1) + Number(num2)
+//   return sums
+// }
+// var result = addNum()
+// console.log(result)
+
+                                                                // Question: 04
+// function putNumber(num) {
+//     var input = document.getElementById("number")
+//     input.value += num
+// }
+// function answer() {
+//     var inputValue = document.getElementById("number")
+//     var output = eval(inputValue.value)
+//     inputValue.value = output
+// }
+// function clearInput() {
+//   document.getElementById("number").value = ""
+// }
+// function clearLastChar() {
+//   var input = document.getElementById("number")
+//   number.value = number.value.slice(0, -1)
+// }
+
+                                                                // Question: 05
+// function squareNum(num1) {
+//   return num1 * num1
+// }
+// var num1 = prompt("Enter a Number and take answer in square")
+// var result = squareNum(num1)
+// console.log(result)
+
+                                                                // Question: 06
+// function factorial(num) {
+//   if (num === 0) {
+//     return 1;
+//   } else {
+//     return num * factorial(num - 1);
+//   }
+// }
+// console.log(factorial(5))
+
+                                                                // Question: 07
+// function countNumbers(start, end) {
+//   if (start > end) {
+//     document.write("Invalid input! The start number should be less than or equal to the end number.");
+//   } else {
+//     for (let i = start; i <= end; i++) {
+//       document.write(i + "<br>");
+//     }
+//   }
+// }
+// countNumbers(prompt("Enter start Number.") , prompt("Enter end Number."))
+
+                                                                // Question: 08
+
+                                                                // Undefined :(
+
+                                                                // Question: 09
+
+
+
